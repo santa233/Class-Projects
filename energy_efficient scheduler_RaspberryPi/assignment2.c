2cf0f8d761ffbe07559482e038254c8bf6e681c3
santa233 <k.santosh23.92@gmail.com>
2017-06-09 04:03:08 +0530

    Create assignment2.c

--- energy_efficient scheduler_RaspberryPi/assignment2.c
+++ energy_efficient scheduler_RaspberryPi/assignment2.c
@@ -19,7 +19,7 @@ void learn_workloads(SharedVariable* sv) {
 	// This function is executed before the scheduling simulation.
 	// You need to calculate the execution time of each thread here.
 
-	// Thread functions for workloads: 
+	// Thread functions for workloads:
 	// thread_button, thread_twocolor, thread_temp, thread_track,
 	// thread_shock, thread_rgbcolor, thread_aled, thread_buzzer
 
@@ -28,10 +28,299 @@ void learn_workloads(SharedVariable* sv) {
 
 	// Tip 2. You can get the current time here like:
 	// long long curTime = get_current_time_us();
-        long long curTime_before = get_current_time_us();
-	thread_button(sv);
-        long long curTime_after = get_current_time_us();
-	printDBG("exe_time_button=%u ",(curTime_after-curTime_before));
+	set_by_min_freq();
+	long long metric;
+        long long curTime_before = get_current_time_us(),max;
+        long long curTime_after = get_current_time_us()-curTime_before;
+	int i;
+
+	max=0;
+	for(i=0;i<20;i++)
+	{
+        	curTime_before=0;
+        	curTime_after=0;
+        	curTime_before = get_current_time_us();
+		thread_button(sv);
+       	 	curTime_after = get_current_time_us()-curTime_before;
+		if(max<curTime_after)
+		{
+			max=curTime_after;
+		}
+                sv->exetime_low[0]=max;
+	}
+
+	max=0;
+        for(i=0;i<20;i++)
+	{
+		curTime_before=0;
+		curTime_after=0;
+		curTime_before = get_current_time_us();
+       	 	thread_twocolor(sv);
+       		curTime_after = get_current_time_us()-curTime_before;
+        	//sv->exetime_low[1]=curTime_after;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_low[1]=max;
+	}
+
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_temp(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_low[2]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_track(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+               // sv->exetime_low[1]=curTime_after;
+                if(max<curTime_after)
+		{
+                        max=curTime_after;
+                }
+                sv->exetime_low[3]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_shock(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_low[4]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_rgbcolor(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+               // sv->exetime_low[1]=curTime_after;
+                if(max<curTime_after)
+		{
+                        max=curTime_after;
+                }
+                sv->exetime_low[5]=max;
+        }
+
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_aled(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_low[6]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_buzzer(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+               // sv->exetime_low[1]=curTime_after;
+                if(max<curTime_after)
+		{
+                        max=curTime_after;
+                }
+                sv->exetime_low[7]=max;
+        }
+
+        set_by_max_freq();
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_button(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                if(max<curTime_after)
+		{
+                        max=curTime_after;
+                }
+                sv->exetime_high[0]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_twocolor(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                //sv->exetime_high[1]=curTime_after;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_high[1]=max;
+        }
+
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_temp(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_high[2]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_track(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+               // sv->exetime_high[1]=curTime_after;
+                if(max<curTime_before)
+		{
+                        max=curTime_after;
+                }
+                sv->exetime_high[3]=max;
+        }
+
+	 max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_shock(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_high[4]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_rgbcolor(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+               // sv->exetime_high[1]=curTime_after;
+                if(max<curTime_after)
+		{
+                        max=curTime_after;
+                }
+                sv->exetime_high[5]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_aled(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+                if(max<curTime_after)
+		{
+                        max=curTime_after ;
+                }
+                sv->exetime_high[6]=max;
+        }
+
+        max=0;
+        for(i=0;i<20;i++)
+	{
+                curTime_before=0;
+                curTime_after=0;
+                curTime_before = get_current_time_us();
+                thread_buzzer(sv);
+                curTime_after = get_current_time_us()-curTime_before;
+               // sv->exetime_low[1]=curTime_after;
+                if(max<curTime_after)
+		{
+                        max=curTime_after;
+                }
+                sv->exetime_high[7]=max;
+	}
+
+//	int i;
+	for(i=0;i<NUM_TASKS;i++)
+	{
+		if( ( (float)sv->exetime_high[i] )/10000 > sv->exetime_high[i]/10000)
+			sv->b[i]=sv->exetime_high[i]/10000+1;
+		else
+			sv->b[i]=sv->exetime_high[i]/10000;
+                if( ( (float)sv->exetime_low[i] )/10000 > sv->exetime_low[i]/10000)
+                        sv->ab[i]=sv->exetime_low[i]/10000+1;
+                else
+                        sv->ab[i]=sv->exetime_low[i]/10000;
+		sv->a[i]=(float)sv->ab[i]/(float)sv->b[i];
+		if(sv->a[i]>2)
+			sv->select[i]=1;
+		else
+			sv->select[i]=0;
+	}
+	//for(i=0;i<NUM_TASKS;i++)
+	//{
+	//	printDBG(" %lld , %lld , %d \n",sv->exetime_low[i],sv->exetime_high[i],sv->select[i]);
+	//}
+	set_by_max_freq();
+	float metic,critical,max1;
+	max1=0;
+	for(i=0;i<NUM_TASKS;i++)
+	{
+		metric=(float)sv->exetime_low[i]/(float)workloadDeadlines[i];
+		if(max1<metric)
+		{
+			max1=metric;
+			critical=i;
+		}
+	}
+	sv->critical=critical;
+	sv->prev_time=get_current_time_us();
 }
 
 
@@ -47,7 +336,7 @@ void learn_workloads(SharedVariable* sv) {
 TaskSelection select_task(SharedVariable* sv, const int* aliveTasks, long long idleTime) {
 	// TODO: Fill the body
 	// This function is executed inside of the scheduling simulation.
-    // You need to implement an energy-efficient EDF (Earliest Deadline First) scheduler.
+        // You need to implement an energy-efficient EDF (Earliest Deadline First) scheduler.
 
 	// Tip 1. You may get the current time elapsed in the scheduler here like:
 	// long long curTime = get_scheduler_elapsed_time_us();
@@ -59,37 +348,128 @@ TaskSelection select_task(SharedVariable* sv, const int* aliveTasks, long long i
 	// It selects a next thread using aliveTasks.
 	static int prev_selection = -1;
 	int i = prev_selection+1,p;
-	long long next_deadline[NUM_TASKS],curr_time,ED;
-	//while()
-	//int i = prev_selection + 1;
-	/*while(1) {
+	long long next_deadline[NUM_TASKS],curr_time,EarliestDeadline,slack,minslack,total,FarthestDeadline,maxslack,extime=0,diff=0;
+	//static long long time_for_energy=get_current_time_us();
+	float energy=0;
+	while(1)
+	{
 		if (i == NUM_TASKS)
 			i = 0;
 
 		if (aliveTasks[i] == 1) {
-		//	prev_selection = i;
 			break;
 		}
 		++i;
-	}*/	
+	}
+	//printf("hi\n");
 	curr_time=get_current_time_us();
-	int min=-1;
-	ED= 9223372036854775807
+//        static long long time_for_energy=curr_time;
+	int min = -1;
+	int  max=-1;
+	int keep[8];
+	EarliestDeadline= 9223372036854775807;
+	FarthestDeadline=0;
+	total=0;
+	minslack=EarliestDeadline;
+	maxslack=0;
+//	printDBG("[%d %d]",aliveTasks[0],aliveTasks[1]);
 	for(p=0;p<NUM_TASKS;p++)
 	{
-		if(aliveTasks[i]==1)
-			next_deadline[i]=curr_time+workloadDeadlines[i];
-		if(ED>next_deadline[i])
+		if(aliveTasks[p]==1)
 		{
-			ED=next_deadline[i];
-			min=i;
+			if(sv->prev_alive_tasks[p]==0)
+				sv->next_deadline[p]=curr_time+workloadDeadlines[p];
+			slack=sv->next_deadline[p]-curr_time;
+			if(minslack>slack)
+			{
+				minslack=slack;
+				EarliestDeadline=sv->next_deadline[p];
+				min=p;
+//				printf("%d",min);
+			}
+			if(maxslack<slack)
+			{
+				maxslack=slack;
+				FarthestDeadline=sv->next_deadline[p];
+				max=p;
+			}
+		}
+		keep[p]=sv->prev_alive_tasks[p];
+		sv->prev_alive_tasks[p]=aliveTasks[p];
+	}
+//	printDBG("%d\n",min);
+        curr_time=get_current_time_us();
+	prev_selection=min;
+	int k,no;
+	for(k=0;k<NUM_TASKS;k++)
+	{
+		if(aliveTasks[k]==1)
+		{
+			no=(FarthestDeadline-curr_time)/workloadDeadlines[k];
+			if((FarthestDeadline-curr_time) < workloadDeadlines[k])
+				no=1;
+		}
+		else
+			no=(FarthestDeadline-curr_time)/workloadDeadlines[k];
+		total=total+no*sv->exetime_low[k];
+
+	}
+	total=total-(sv->exetime_high[min])+(sv->exetime_low[min]);
+	TaskSelection sel;
+	sel.task=prev_selection;
+	if(total<((FarthestDeadline-curr_time)+10000))
+	{
+		if(sv->select[prev_selection]==0)
+		{
+			sel.freq=1;
+			sv->low++;
+		}
+		else
+		{
+			sel.freq=1;
+			sv->high++;
 		}
 	}
-	prev_selection=min;
-	// The retun value can be specified like this:
-	TaskSelection sel;
-	sel.task = prev_selection; // The thread ID which will be scheduled. i.e., 0(BUTTON) ~ 7(BUZZER)
-	sel.freq = 1; // Request the maximum frequency (if you want the minimum frequency, use 0 instead.)
-
-    return sel;
+	else
+	{
+        	if( ((int)(sv->exetime_low[prev_selection] - 10000/sv->a[prev_selection])/10000) < sv->ab[prev_selection] && keep[prev_selection]==0)
+		{
+			if(sv->select[prev_selection]==0)
+			{
+				sel.freq=1;
+				sv->low++;
+			}else
+			{
+				sel.freq=1;
+				sv->high++;
+			}
+		}
+		else
+		{
+			sel.freq=1;
+			sv->high++;
+		}
+	}
+	sv->idle=sv->idle+idleTime;
+//	printDBG("%d",sel.task);
+	if(sv->ii==0)
+	{
+		extime=0;
+		sv->ii++;
+	}
+	else
+        	extime=curr_time-sv->prev_time-idleTime;
+        if(sel.freq==0)
+               sv->energy=sv->energy+extime*0.4+idleTime*0.05;
+        else
+               sv->energy=sv->energy+extime*0.8+idleTime*0.05;
+//	if(idleTime>0)
+//	printDBG("energy=%lld exe_time  %lld idle time %lld \n",sv->energy,extime,sv->idle);
+	sv->prev_time=get_current_time_us();
+//	TaskSelection sel;
+//	sel.task=0;
+//	sel.freq=1;
+//	printDBG("task = %d\n",prev_selection);
+	return sel;
 }
+
